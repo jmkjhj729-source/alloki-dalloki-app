90f320878af8de4929b374682d810090888c9391
jmkjhj729-source <jmkjhj729@gmail.com>
2026-01-28 22:21:20 +0900

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="알록이 & 달록이", layout="centered")
+
+st.title("🐼 알록이 & 달록이 앱")
+st.write("Streamlit 배포 성공 🎉")
+
+st.info("이제 여기에 UI를 하나씩 붙이면 됩니다.")
